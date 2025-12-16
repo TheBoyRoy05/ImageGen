682995abacd2a433e404b00d4749b91a3c610dd3
Issac Roy <issacroy05@gmail.com>
2025-12-16 11:17:24 -0800

    fixed bugs

--- modules.py
+++ modules.py
@@ -101,13 +101,12 @@ class UNet(nn.Module):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
-
         self.inc = DoubleConv(c_in, 64)
         self.down1 = Down(64, 128)
         self.sa1 = SelfAttention(128, 32)
         self.down2 = Down(128, 256)
         self.sa2 = SelfAttention(256, 16)
-        self.down3 = Down(256, 512)
+        self.down3 = Down(256, 256)
         self.sa3 = SelfAttention(256, 8)
 
         self.bot1 = DoubleConv(256, 512)
@@ -116,14 +115,17 @@ class UNet(nn.Module):
 
         self.up1 = Up(512, 128)
         self.sa4 = SelfAttention(128, 16)
-        self.up2 = Up(128, 64)
+        self.up2 = Up(256, 64)
         self.sa5 = SelfAttention(64, 32)
         self.up3 = Up(128, 64)
         self.sa6 = SelfAttention(64, 64)
         self.outc = nn.Conv2d(64, c_out, kernel_size=1)
 
     def pos_encoding(self, t, channels):
-        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device) / channels))
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
         pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
         pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
         pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
@@ -133,7 +135,7 @@ class UNet(nn.Module):
         t = t.unsqueeze(-1).type(torch.float)
         t = self.pos_encoding(t, self.time_dim)
 
-        x1 = self.inc1(x)
+        x1 = self.inc(x)
         x2 = self.down1(x1, t)
         x2 = self.sa1(x2)
         x3 = self.down2(x2, t)
@@ -147,11 +149,12 @@ class UNet(nn.Module):
 
         x = self.up1(x4, x3, t)
         x = self.sa4(x)
-        x4 = self.up2(x, x2, t)
+        x = self.up2(x, x2, t)
         x = self.sa5(x)
         x = self.up3(x, x1, t)
         x = self.sa6(x)
-        return self.outc(x)
+        output = self.outc(x)
+        return output
 
 class Diffusion:
     def __init__(
